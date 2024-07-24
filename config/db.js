f21dd6c86b7011eb55f8edf137d190149775f68b
KK-tech-byte1991 <84914086+KK-tech-byte1991@users.noreply.github.com>
2024-07-24 19:04:29 +0530

    live dbase

--- config/db.js
+++ config/db.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/form-bot', {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
+        // await mongoose.connect('mongodb://localhost:27017/form-bot', {
+        //     // useNewUrlParser: true,
+        //     // useUnifiedTopology: true,
+        // });
+        // mongodb+srv://kishor:Durva@cluster0.mirndhq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+        await mongoose.connect('mongodb+srv://kishor:Durva@cluster0.mirndhq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/form-bot', {           
         });
         console.log('MongoDB connected');
     } catch (err) {
