4c3f54d16483621c351b02395effcf3beb92a0cf
KK-tech-byte1991 <84914086+KK-tech-byte1991@users.noreply.github.com>
2024-07-24 18:55:41 +0530

    vercel deployment chnages

--- app.js
+++ app.js
@@ -12,3 +12,4 @@ app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
+module.exports = app;
--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect('mongodb://localhost:27017/form-bot', {
-            useNewUrlParser: true,
+            // useNewUrlParser: true,
             // useUnifiedTopology: true,
         });
         console.log('MongoDB connected');
