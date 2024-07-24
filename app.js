6dcff9db8ba35c755ba3fff6d44a6fc039fc0121
KK-tech-byte1991 <84914086+KK-tech-byte1991@users.noreply.github.com>
2024-07-24 20:18:36 +0530

    env variables

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors'); 
-
+require('dotenv').config();
 const app = express();
 
 connectDB();
--- config/db.js
+++ config/db.js
@@ -2,13 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect('mongodb://localhost:27017/form-bot', {
-        //     // useNewUrlParser: true,
-        //     // useUnifiedTopology: true,
-        // });
-        // mongodb+srv://kishor:Durva@cluster0.mirndhq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-        await mongoose.connect('mongodb+srv://kishor:Durva@cluster0.mirndhq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/form-bot', {           
-        });
+        await mongoose.connect(process.env.MONGO_URI, {         
+        });       
+
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err.message);
