856a39a0242798e3780cc25331c8b682f9195b55
Kishor <kishorkumarmali1991@gmail.com>
2024-07-31 22:01:59 +0530

    removed tranform

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-const transformIdMiddleware = require('./transformIdMiddleware');
+// const transformIdMiddleware = require('./transformIdMiddleware');
 const app = express();
 const { authMiddleware } = require("./controllers/authController")
 connectDB();
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(transformIdMiddleware);
+// app.use(transformIdMiddleware);
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/folder', authMiddleware, require('./routes/folder'));
